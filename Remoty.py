ba924c39c874fdc6e462e5e9d064eaa50d1451fa
hack3r3 <75802891+hack3r3@users.noreply.github.com>
2020-12-10 18:40:22 -0300

    Update Remoty.py

--- Remoty.py
+++ Remoty.py
@@ -3,19 +3,19 @@ import re
 from bs4 import BeautifulSoup
 import sys
 
+
 header = {'user-agent: "Mozilla/5.0 (X11; Linux 1686; rv:43.0) Gecko/10100101 Firefox/43.0 Iceweasel/43.0.4'}                        
 
-site = sys.arg[1]
+site = sys.argv[1]
 req = requests.get(site, data=header)
 html = req.text
 
 bs = BeautifulSoup(html, 'lxml')
 
-exp = re.findall(r'[\w.]+[\w-]+[\w_]@[]+[\w.]+[(yahoo.)?]+[(gmail.)?]', html) #r = raw  string
-v = bs.img['src']
+exp = re.findall(r'[\w.]+[\w-]+[\w_]@[\w.]+[(yahoo.)?]+[(gmail.)?]', html) #r = raw  string
+v = bs4.img['src']
 
 for email in exp:
-	print('Email > '+str(email))
+	print('Email > ' + str(email))
 
 	print('External Links: ' + str(v))
-	
